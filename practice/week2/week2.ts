6730f6d2705d8b5528f5186367e6f2d7f2691d22
w.h heo <34547196+her0707@users.noreply.github.com>
2023-08-20 07:48:02 +0900

    [실습] 연습문제 week2 풀이 (#4)
    * chore: tsc build script add
    
    * feat: week2 문제 풀이
    
    * refactor: lint error fix
    
    * refactor: lint fix

--- practice/week2/week2.ts
+++ practice/week2/week2.ts
@@ -1,14 +1,15 @@
+/* eslint-disable  */
 /**
  * 카피 온 라이트 적용해보기
  */
 
 const mailingList = [];
 
-function addContact(email) {
+function addContact(email: any) {
   mailingList.push(email);
 }
 
-function submitFormHandler(event) {
+function submitFormHandler(event: { target: any }) {
   const form = event.target;
   const email = form.elements["email"].value;
   addContact(email);
