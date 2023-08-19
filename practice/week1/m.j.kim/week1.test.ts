04ee1c2f82addc424c7d40783e89fd1637203079
ring-wdr <akswnd55@gmail.com>
2023-08-19 18:22:36 +0900

    move folder

---
+++ practice/week1/m.j.kim/week1.test.ts
@@ -0,0 +1,12 @@
+import calcCartTotal from "./week1";
+
+test("wee1 test", () => {
+  const logSpy = jest.spyOn(console, "log");
+
+  calcCartTotal();
+
+  expect(logSpy).toHaveBeenCalledWith("hide");
+  expect(logSpy).toHaveBeenCalledWith("show");
+  expect(logSpy).toHaveBeenCalledWith("hide");
+  expect(logSpy).toHaveBeenCalledWith("1.2");
+});
