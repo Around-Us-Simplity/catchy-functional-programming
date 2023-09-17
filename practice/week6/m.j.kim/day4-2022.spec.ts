b5d99564738932251e7995b6463bf209190e70bd
ring-wdr <akswnd55@gmail.com>
2023-09-17 17:34:30 +0900

    week6

--- practice/week6/m.j.kim/day4-2022.spec.ts
+++ practice/week6/m.j.kim/day4-2022.spec.ts
@@ -1,30 +1,18 @@
 import * as path from "path";
-import { isContain, isOverlap, processFile } from "./day4-2022";
+import * as fs from "fs";
+import { firstReducer, secondReducer } from "./day4-2022";
 
 describe("first day", () => {
   const inputPath = path.join(__dirname, "..", "day4-2022.txt");
+  const input = fs.readFileSync(inputPath).toString().split("\n");
 
   it("first answer", async () => {
-    const firstAnswer = await processFile(
-      inputPath,
-      (line: string) => {
-        const [[sStart, sEnd], [eStart, eEnd]] = line.split(",").map(st => st.split("-").map(el => parseInt(el)));
-        return isContain(sStart, sEnd, eStart, eEnd);
-      },
-      0,
-    );
+    const firstAnswer = input.reduce(firstReducer, 0);
     expect(firstAnswer).toEqual(567);
   });
 
   it("second answer", async () => {
-    const secondAnswer = await processFile(
-      inputPath,
-      (line: string) => {
-        const [[sStart, sEnd], [eStart, eEnd]] = line.split(",").map(st => st.split("-").map(el => parseInt(el)));
-        return isOverlap(sStart, sEnd, eStart, eEnd);
-      },
-      0,
-    );
+    const secondAnswer = input.reduce(secondReducer, 0);
     expect(secondAnswer).toEqual(907);
   });
 });
--- practice/week6/m.j.kim/day4-2022.ts
+++ practice/week6/m.j.kim/day4-2022.ts
@@ -1,29 +1,18 @@
-import * as fs from "fs";
-import * as readline from "readline";
+type ValidatorParams = typeof isContain;
 
-// 한 줄씩 읽어들이는 함수 정의
-export async function processFile(filename: string, cbLine: (line: string) => boolean | undefined, initValue: number) {
-  return new Promise<number>(resolve => {
-    const instream = fs.createReadStream(filename);
-    const reader = readline.createInterface(instream, process.stdout);
-    let val = initValue;
-
-    // 한 줄씩 읽어들인 후에 발생하는 이벤트
-    reader.on("line", (line: string) => {
-      if (!line) return;
-      cbLine(line) && val++;
-    });
-
-    // 모두 읽어들였을 때 발생하는 이벤트
-    reader.on("close", () => {
-      resolve(val);
-    });
-  });
-}
-
-export const isContain = (sStart: number, sEnd: number, eStart: number, eEnd: number) => {
+const isContain = (sStart: number, sEnd: number, eStart: number, eEnd: number) => {
   return (sStart <= eStart && sEnd >= eEnd) || (sStart >= eStart && sEnd <= eEnd);
 };
 
-export const isOverlap = (sStart: number, sEnd: number, eStart: number, eEnd: number) =>
+const isOverlap = (sStart: number, sEnd: number, eStart: number, eEnd: number) =>
   (sStart <= eStart && eStart <= sEnd) || (eStart <= sStart && sStart <= eEnd);
+
+const originReducer = (cb: ValidatorParams) => (acc: number, str: string) => {
+  if (!str) return acc;
+  const [[sStart, sEnd], [eStart, eEnd]] = str.split(",").map(st => st.split("-").map(el => parseInt(el)));
+  return cb(sStart, sEnd, eStart, eEnd) ? acc + 1 : acc;
+};
+
+export const firstReducer = originReducer(isContain);
+
+export const secondReducer = originReducer(isOverlap);
