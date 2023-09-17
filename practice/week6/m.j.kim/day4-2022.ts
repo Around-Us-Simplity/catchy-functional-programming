0d0ac87cb43284040cd5e0b22ccb820220813289
ring-wdr <akswnd55@gmail.com>
2023-09-17 17:41:05 +0900

    trivial

--- practice/week6/m.j.kim/day4-2022.ts
+++ practice/week6/m.j.kim/day4-2022.ts
@@ -1,8 +1,7 @@
 type ValidatorParams = typeof isContain;
 
-const isContain = (sStart: number, sEnd: number, eStart: number, eEnd: number) => {
-  return (sStart <= eStart && sEnd >= eEnd) || (sStart >= eStart && sEnd <= eEnd);
-};
+const isContain = (sStart: number, sEnd: number, eStart: number, eEnd: number) =>
+  (sStart <= eStart && sEnd >= eEnd) || (sStart >= eStart && sEnd <= eEnd);
 
 const isOverlap = (sStart: number, sEnd: number, eStart: number, eEnd: number) =>
   (sStart <= eStart && eStart <= sEnd) || (eStart <= sStart && sStart <= eEnd);
